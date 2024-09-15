378dce060731c8f0e05db95122d767e5894f5433
Lautaro65 <lautarofnewells@gmail.com>
2024-09-15 13:30:20 -0300

    ESLint VideoToImage9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,11 @@ import { FaPause } from "react-icons/fa";
 import { FaPlay } from "react-icons/fa";
 import { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import CarouselSection from "./components/carruselSection/page";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
@@ -42,7 +46,7 @@ export default function Home() {
       }
     }
   };
-  const images = [
+  const imageSrcs: string[] = [
     "/image/swiper/Acapulco.jpg",
     "/image/swiper/Incitadores.jpg",
     "/image/swiper/LaMujerEnElLago.jpg",
@@ -52,33 +56,18 @@ export default function Home() {
     "/image/swiper/Pachinko.jpg",
     "/image/swiper/SePresume.jpg",
   ];
-  const texts = [
-    { title: "Comedia", description1: "Todo es posible en el paraíso" },
-    { title: "Acción", description1: "El peor robo del mundo." },
-    {
-      title: "Suspenso",
-      description1:
-        "Natalie Portman y Moses Ingram protagonizan un visionario thriller.",
-    },
-    {
-      title: "Crimen",
-      description1:
-        "La historia inspirada en crímenes reales y las mujeres que los resolvieron.",
-    },
-    {
-      title: "Ciencia ficción",
-      description1: "Una vida, posibilidades infinitas.",
-    },
-    {
-      title: "Drama",
-      description1: "Disfruta la versión extendida de Ridley Scott.",
-    },
-    {
-      title: "Drama",
-      description1: "El valor para cambiar tu destino está en ti.",
-    },
-    { title: "Crimen", description1: "Nueva serie." },
+
+  const texts: { title: string; description1: string }[] = [
+    { title: "Título 1", description1: "Descripción 1" },
+    { title: "Título 2", description1: "Descripción 2" },
+    { title: "Título 3", description1: "Descripción 3" },
+    { title: "Título 4", description1: "Descripción 4" },
+    { title: "Título 5", description1: "Descripción 5" },
+    { title: "Título 6", description1: "Descripción 6" },
+    { title: "Título 7", description1: "Descripción 7" },
+    { title: "Título 8", description1: "Descripción 8" },
   ];
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <main>
@@ -569,7 +558,56 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <CarouselSection imageSrcs={images} texts={texts} />
+      <section className="w-full h-[85vh] p-2 bg-white">
+        <Swiper
+          loop={true}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          pagination={{ clickable: true }}
+          spaceBetween={20} // Espacio entre slides
+          slidesPerView={1.5} // Ver 1 slide completo y parte de otros
+          centeredSlides={true} // Centra el slide activo
+          onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)} // Actualiza el índice activo
+          className="h-full " // Ajusta la altura del swiper
+          modules={[Pagination]}
+        >
+          {imageSrcs.map((src, index) => (
+            <SwiperSlide key={index} className="relative">
+              <div className="relative h-[95%] w-full">
+                <Image
+                  src={src}
+                  alt={`Slide ${index + 1}`}
+                  layout="fill"
+                  objectFit="cover"
+                  quality={100}
+                />
+                {index !== activeIndex && (
+                  <div className="absolute inset-0 bg-gray-500 bg-opacity-50"></div>
+                )}
+                {index === activeIndex && (
+                  <div className="absolute inset-0 flex items-center justify-center text-center p-4">
+                    <div className="flex flex-col items-center justify-between w-full">
+                      <div className="flex items-center w-full absolute bottom-16 pl-16 gap-5">
+                        <button className="bg-white text-black py-3 px-6 rounded-full">
+                          Ver ahora
+                        </button>
+                        <div className="flex gap-5 items-center">
+                          <p className="text-white mb-1 text-xl font-semibold">
+                            {texts[index]?.title || "Texto 1"}
+                          </p>
+                          <p className="text-white text-lg">
+                            {texts[index]?.description1 || "Texto 2"}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </SwiperSlide>
+          ))}
+          <div className="swiper-pagination"></div>
+        </Swiper>
+      </section>
     </main>
   );
 }
