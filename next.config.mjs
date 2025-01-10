04556e049f5c9b4ab8af01a3807d089143d5ba18
Lautaro Octavio Faure <98252667+lautaro65@users.noreply.github.com>
2025-01-10 00:04:55 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,18 @@ const nextConfig = {
           },
         ],
       },
+     async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
+          { key: 'Content-Security-Policy', value: 'frame-ancestors *' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
